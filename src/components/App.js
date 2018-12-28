bad7da4cf48314f470a994f35fc5b920606ea53a
David Benavidez <benavidez_david13@yahoo.com>
2018-12-27 23:26:19 -0800

    added animation

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,41 @@ import logo from '../logo.svg';
 import Work from './work/WorkModal';
 import Contact from './contact/ContactModal';
 import About from './about/AboutModal';
+import { CSSTransition } from 'react-transition-group';
 
 import '../App.css';
 
 class App extends Component {
-  
+  state = { appearHome: true };
+
   render() {
     return (
       <div className="Home">
-        <div className="Header">
-          <h1> Full Stack Developer </h1>
-        </div>
-        <div className="Line" />
-        <div className="Buttons">
-          <Work />
-          <About />
-          <Contact />
-        </div>
+        <CSSTransition
+          in={this.state.appearHome}
+          appear={true}
+          timeout={200}
+          classNames="fade"
+        >
+          <div className="Homes">
+            <CSSTransition
+              in={this.state.appearHome}
+              appear={true}
+              timeout={400}
+              classNames="fade"
+            >
+              <div className="Header">
+                <h1> Full Stack Developer </h1>
+              </div>
+            </CSSTransition>
+            <div className="Line" />
+            <div className="Buttons">
+              <Work />
+              <About />
+              <Contact />
+            </div>
+          </div>
+        </CSSTransition>
       </div>
     );
   }
